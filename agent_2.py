3c574976b2332052c19e99289a5eb5a988cd6d7e
nathan <76900734+nathanjstack@users.noreply.github.com>
2026-09-20 15:04:25 +0800

    commented out lines in agents_2 in order to make compatible with windows

--- agent_2.py
+++ agent_2.py
@@ -1,5 +1,5 @@
 import time
-import timeout_decorator
+# import timeout_decorator  # Commented out for Windows compatibility
 '''
 WINDOWS COMPATIBILITY NOTE:
     The timeout_decorator package may not work correctly on Windows. For local
@@ -21,62 +21,29 @@ class StudentAgent(Agent):
     You can add/override attributes and methods as needed.
     '''
 
-    @timeout_decorator.timeout(1)
+    # @timeout_decorator.timeout(1)  # Commented out for Windows compatibility
     def __init__(self, agent_name='Give a nickname'):
         super().__init__(agent_name)
 
         '''Implement your agent here.'''
 
-    @timeout_decorator.timeout(1)
+    # @timeout_decorator.timeout(1)  # Commented out for Windows compatibility
     def new_game(self, game, power_name):
         self.game = game
         self.power_name = power_name
 
         '''Implement your agent here.'''
 
-    @timeout_decorator.timeout(1) # This is only for updating the game engine and other states if any. Do not implement heavy stratergy here.
+    # @timeout_decorator.timeout(1)  # Commented out for Windows compatibility
     def update_game(self, all_power_orders):
         # do not make changes to the following codes
         for power_name in all_power_orders.keys():
             self.game.set_orders(power_name, all_power_orders[power_name])
         self.game.process()
 
-    @timeout_decorator.timeout(1)
+    # @timeout_decorator.timeout(1)  # Commented out for Windows compatibility
     def get_actions(self):
 
         '''Implement your agent here.'''
         
         return [] 
-
-        '''
-        Return a list of orders. Each order is a string, with specific format. For the format, read the game rule and game engine documentation.
-        
-        Expected format:
-        A LON H                  # Army at LON holds
-        F IRI - MAO              # Fleet at IRI moves to MAO (and attack)
-        A WAL S F LON            # Army at WAL supports Fleet at LON (and hold)
-        F NTH S A EDI - YOR      # Fleet at NTH supports Army at EDI to move to YOR
-        F NWG C A NWY - EDI      # Fleet at NWG convoys Army at NWY to EDI
-        A NWY - EDI VIA          # Army at NWY moves to EDI via convoy
-        A WAL R LON              # Army at WAL retreats to LON
-        A LON D                  # Disband Army at LON
-        A LON B                  # Build Army at LON
-        F EDI B                  # Build Fleet at EDI
-
-        Note: If an invalid order is sent to the engine, it will be accepted but with a result of 'void' (no effect).
-        Note: For a 'support' action, two orders are needed, one for the supporter and one for the supportee. (Same for 'convoy')
-        Note: For each unit, if no order is given, it will 'hold' by default.
-
-        Useful Functions:
-        
-        # This is a dict of all the possible orders for each unit at each location (for all powers).
-        possible_orders = self.game.get_all_possible_orders()
-
-        # This is a list of all orderable locations for the power you control.
-        orderable_locations = self.game.get_orderable_locations(self.power_name)
-    
-        # Combining these two, you can have the full action space for the power you control.
-
-        # You can re-use the build_map_graphs function in the GreedyAgent to build the connection graph of the map if needed.
-        
-        '''
